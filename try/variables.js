ecbe67eac2706f4e4809a4bdbc57d4e5adc20b2a
joseMartinez07 <jdaniel.mcaceres@gmail.com>
2024-01-02 16:16:29 -0500

    clase 4 ¿Qué es una variable?

---
+++ try/variables.js
@@ -0,0 +1,17 @@
+var nombre = "Hola mundo"; //sintaxis de una variable
+
+// declarar 
+var edad;
+
+// inicializar
+edad = 29;
+
+// declarando e inicializando una variable de tipo objeto array
+var elementos = ["Computadora","Telefono","teclado"];
+
+// declarando e inicializando una variable de tipo objeto 
+var persona = {
+    nombre:"Rodrigo",
+    apellido:"Martinez",
+    adad:28
+};
