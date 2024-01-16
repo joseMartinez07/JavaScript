b5ec13597551b617e25ed1ad2841466fa1132cfc
joseMartinez07 <jdaniel.mcaceres@gmail.com>
2024-01-16 12:47:10 -0500

    Clase 5 Qué son las funciones en JavaScript

---
+++ try/Funciones.js
@@ -0,0 +1,18 @@
+// Declarativas
+function miFuncion(){
+    var valor=3;
+    return valor;
+}
+// Expreción
+
+var otramifuncion = function(a,b){
+    return a+blur;   
+}
+
+//EJERCICIOS CON LA CONSOLA DEL NAVEGADOR
+
+function saludoEstudiantes(estudiantes){
+    console.log(`Hola ${estudiantes}`); // esto concatena lo que tenga la variable estudiande con hola 
+}
+
+saludoEstudiantes("Diego"); // en esta linea se llama la función 
